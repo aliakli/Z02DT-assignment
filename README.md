caa460849a41381ce20f42266abb9c6c190c1ee8
aliakli <aklia@coventry.ac.uk>
2026-01-22 17:12:48 +0000

    Artefact 2   additional functions (#1)
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Delete Artefact 1 AA.pdf
    
    * Add files via upload
    
    * Add files via upload
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update main.py
    
    * Update operators.py
    
    * Update main.py
    
    * Update operators.py

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Z02DT-assignment
+## Artefact 1 
+Basic arithmetical functions and memory
+## Artefact 2 
+Additional functions (Trig, exponentiation, logarithms, nth root, and mathematical constants)
--- main.py
+++ main.py
@@ -1,156 +1,84 @@
 #14/01
 #Imports
-import math
-
+from operators import Operators
+from NumberValidation import NumberValidation
 #Global variables
-NOT_A_NUMBER = "NaN" 
-ANSWER: int | None = None
+
 
 
 #Main
 def main():
-    #Call global ANSWER variable
-    global ANSWER
-    #Main program loop 
-    while True: 
-        #Check whether user wants to continue using the calculator, clear memory or exit (strip to remove whitespace and move input to lower case) 
-        operation = input("Enter an operator (+,-,*,/), enter 'clr' to delete memory, enter 'ans' to see the answer stored in memory or press enter to exit: ").lower().strip() 
-        #End main program loop if requested to 
-        if operation == "": 
-            print("Goodbye") 
-            break
-        
-        #Store the operation in the variable 'operation'
-        operation = handle_operator(operation) 
-        if operation == "Not an operator": 
-            print("Invalid operator") 
-            continue
-        elif operation == "clear":
-            print(ANSWER)
-            continue
-        elif operation == "memory":
-            continue
-        
-        #Get user number inputs
-        numbers = get_numbers()
-        #Assign the first number to answer then remove it from the first element in the array
-        if len(numbers) == 0:
-            ANSWER = 0 
-        #Perform the operation on the numbers stored in 'numbers'
-        #Display to output the result
-        if operation(numbers) == "Division by zero":
-            print("Division by zero")
-            ANSWER = None
-        else:
-            print(ANSWER) 
+    operators = Operators()
+    valid_nums = NumberValidation()
 
-#Gets the validated numbers from the user input then stores them in an array
-def get_numbers():
-    numbers = []
-    #Loops until the final input is '='
     while True:
-        #Add the validated number to the end of the list
-        numbers.append(get_valid_number())
-        #Checks if the last value in the array is '='
-        if numbers[-1] == "=":
-            #Remove the '='
-            numbers.pop()
+        operation = input("Enter an operator, enter 'help' for help or press enter to exit: ").lower().strip()
+        if operation == "":
+            print("Goodbye")
             break
-    #Return the array
-    return numbers
-            
-#Gets number from user input and validates     
-def get_valid_number():
-    num = input("Enter a number and press '=' to compute: ")
-    #Loops until valid number is given
-    while validate_number(num) == NOT_A_NUMBER:
-        num = input("Not a valid number\nEnter a number: ")
-    return validate_number(num)
-
-#Function to validate number 
-def validate_number(inp):
-    if inp == "=":
-        return "="
-    else:
-        try: 
-            a = float(inp)
-            return int(a) if a.is_integer() else a
-        except ValueError: 
-            #If a number given is not a valid number, this prevents the program from crashing 
-            return NOT_A_NUMBER 
-
+        if operation == "help":
+            print("""
+Enter ans to recall memory
+Enter clr to clear memory\n
+Enter the following for their respective functions:
++/add: addition
+-/sub: subtraction
+*/mult: multiplication
+/ or div: division
+^/exp: exponentiation\n
+Trig (degrees)
+sin
+cos
+tan\n
+log: logarithm
+            """)
+            continue
+        operation = handle_operator(operation, operators)
+        if operation == "Not an operator":
+            print("Invalid operator")
+            continue
+        elif operation is None:
+            continue
+        numbers = valid_nums.get_numbers()
+        if len(numbers) > 0:
+            operators.answer += numbers.pop(0)
+        result = operation(numbers)
+        if result == "Division by zero":
+            print(result)
+            operators.clear()
+        else:
+            print(operators.answer)
 #Function to decide which operator is being requested 
-def handle_operator(operator): 
+def handle_operator(operator,operators): 
     #Using a variety of ways people may ask for an operator to account for the broadest amount of users
-    global ANSWER
     match operator:
         case "mem" | "memory" | "ans" | "answer":
-            print(ANSWER)
-            return "memory"
+            print(operators.memory())
+            return None
         case "cls" | "clear" | "clr" | "del":
-            ANSWER = None
-            return "clear"
+            operators.clear()
+            return None
         case "add" | "addition" | "a" | "+": 
-            return addition 
+            return operators.add 
         case "sub" | "subtraction" | "s" | "-" | "subtract": 
-            return subtraction 
+            return operators.subtraction 
         case "multiply" | "mult" | "m" | "*" | "multiplication": 
-            return multiplication 
+            return operators.multiplication 
         case "division" | "div" | "d" | "/" | "divide": 
-            return division 
+            return operators.division
+        case "exp" | "exponentiation" | "power" | "^" | "**":
+            return operators.exponentiation
+        case "sin" | "sine":
+            return operators.sine
+        case "cos" | "cosine":
+            return operators.cosine
+        case "tan" | "tangent":
+            return operators.tangent
+        case "log" | "logarithm":
+            return operators.log10
         case _: 
             return "Not an operator" 
-
-#Function to add an array of inputs 
-def addition(lst):
-    global ANSWER
-    if ANSWER is None or len(lst) > 1:
-        ANSWER = lst.pop(0)
-    #Output (n+(n+1)+(n+2)...)
-    print(ANSWER,"+"," + ".join("{0}".format(n) for n in lst))
-    for number in lst:
-        ANSWER += number
-
-#Function to subtract 2 inputs 
-def subtraction(lst):
-    global ANSWER
-    if ANSWER is None or len(lst) > 1:
-        ANSWER = lst.pop(0)
-    #Output (n-(n+1)-(n+2)...)
-    print(ANSWER,"-"," - ".join("{0}".format(n) for n in lst))
-    for number in lst:
-        ANSWER -= number
-    
-
-#Function to divide 2 inputs 
-def division(lst):
-    global ANSWER
-    if ANSWER is None or len(lst) > 1:
-        ANSWER = lst.pop(0)
-    try:
-        #Output (n/(n+1)/(n+2)...)
-        print(ANSWER,"/"," / ".join("{0}".format(n) for n in lst))
-        for number in lst:
-            ANSWER /= number
-        ANSWER = validate_number(ANSWER) 
-    #To prevent the program from crashing, this exception catches the ZeroDivisionError and returns the string 
-    except ZeroDivisionError:
-        return "Division by zero" 
-
-  
-
-#Function to multiply 2 inputs 
-def multiplication(lst):
-    global ANSWER
-    if ANSWER is None or len(lst) > 1:
-        ANSWER = lst.pop(0)
-    #Output (n*(n+1)*(n+2)...)
-    print(ANSWER,"*"," * ".join("{0}".format(n) for n in lst))
-    for number in lst:
-        ANSWER *= number
-
 
 #Prevents the program from being ran when imported (probably will use this with OOP) 
 if __name__ == "__main__": 
     main() 
-
