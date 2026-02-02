aa66b8082b8715ad2adfa45f371ff518b62c68da
aliakli <aklia@coventry.ac.uk>
2026-02-02 14:33:05 +0000

    Update main.py
    rewrote main.py to conform with pep8 style standards

--- Artefact 3/main.py
+++ Artefact 3/main.py
@@ -1,22 +1,25 @@
-#30/01
-#Imports
-from bidmas import Calculator
-#Global variables
+# 30/01
 
-#Main
+# Imports
+from bidmas import Calculator
+
 def main():
     instance = Calculator()
+
     while True:
-        
-        expression = input("Enter expression, type exit to close or type mode to change mode:\n")
+        expression = input(
+            "Enter expression, type exit to close or type mode to change mode:\n"
+        )
+
         if expression.lower() == "exit":
             break
-        elif expression.lower() == "mode":
+
+        if expression.lower() == "mode":
             instance.angle_mode = not instance.angle_mode
-            print("Radians" if instance.angle_mode else "Degrees") 
+            print("Radians" if instance.angle_mode else "Degrees")
         else:
             print(instance.tokenise_expression(expression))
-        
-#Prevents the program from being ran when imported
-if __name__ == "__main__": 
-    main() 
+
+
+if __name__ == "__main__":
+    main()
