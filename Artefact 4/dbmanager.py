e9f5a1defdc471137ececd22b1f92ab58b10bcfc
aliakli <aklia@coventry.ac.uk>
2026-02-05 16:28:39 +0000

    Update dbmanager.py
    removed cryptography

--- Artefact 4/dbmanager.py
+++ Artefact 4/dbmanager.py
@@ -1,33 +1,30 @@
 import datetime
 import mysql.connector
-from cryptography.fernet import Fernet
 
 
-k = open("key.txt", "r")
-
-
-def log_change(change, user = "admin"):
-    logs = open("log.txt", "a")
-    now = datetime.datetime.today().replace(microsecond=0)
-    logs.write(f"{user}: {change}: {now}\n")
-    logs.close()
+class Logs:
+    def log_change(change, user = "admin"):
+        logs = open("log.txt", "a")
+        now = datetime.datetime.today().replace(microsecond=0)
+        logs.write(f"{user}: {change}: {now}\n")
+        logs.close()
 
 
 class Database:
-    def __init__(self, target_database):
+    def __init__(self, target_database, aka = "admin"):
         self.host = "localhost"
         self.user = "root"
         self.password = "C0ventryCUC"
         self.cursor = None
         self.db = None
+        self.aka = aka
 
-        if not self.__connect(target_database):
+        if not self.__connect(target_database, aka):
             print(f"failed to connect to {target_database}")
             return
         else:
             self.cursor = self.db.cursor()
-            print(f"successfuly connected to {target_database}")
-            log_change("Connected to database", user = "admin")
+            Logs.log_change("Connected to database", user = aka)
             # test
             # self.__create_database("johnCena")
 
@@ -37,7 +34,7 @@ class Database:
         if name not in cursor:
             try:
                 cursor.execute(f"CREATE DATABASE {name}")
-                log_change(f"Created database called {name}", user = "admin")
+                Logs.log_change(f"Created database called {name}", user = "admin")
             except:
                 print(f"database '{name}' could not be created")
 
@@ -57,9 +54,9 @@ class Database:
         ENGINE = InnoDB
         """
         self.cursor.execute(query)
-        log_change(f"Created table called {table_name}", user = "admin")
+        Logs.log_change(f"Created table called {table_name}", user = "admin")
 
-    def __connect(self, target_database):
+    def __connect(self, target_database, users = "admin"):
         try:
             self.db = mysql.connector.connect(
                 host=self.host,
@@ -67,24 +64,26 @@ class Database:
                 password=self.password,
                 database=target_database
             )
-            log_change(
+            Logs.log_change(
                 f"Successfully connected to database {target_database}",
-                user = "admin"
+                user = users
             )
             return True
         except mysql.connector.Error as err:
             print("MySQL error:", err)
-            log_change(
+            Logs.log_change(
                 f"Failed to connect to database {target_database}",
                 user = "admin"
             )
+            raise ValueError(f"Unable to connect to {target_database}")
             return False
 
 
 class UserManagement(Database):
-    def __init__(self):
-        super().__init__("users")
+    def __init__(self, aka = "system"):
+        super().__init__("users", aka)
         self.__create_user_table()
+        self.aka = aka
 
     def __create_user_table(self):
         cursor = self.cursor
@@ -101,53 +100,56 @@ class UserManagement(Database):
             "role CHAR(255) DEFAULT 'visitor'",
             "added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
         )
-        log_change("Created user table", user = "admin")
+        Logs.log_change("Created user table", user = "admin")
 
-    def create_user(self, username, password, role="visitor", fernet=None):
+    def create_user(self, username, password, role="visitor"):
         roles = ["visitor", "admin", "test"]
         if role not in roles:
             raise Exception("Invalid role")
-
-        if fernet is None:
-            key = k.readline().strip().encode("utf-8")
-            fernet = Fernet(key)
-
-        token = fernet.encrypt(password.encode("utf-8"))
-
-        value = (username, token, role)
+        value = (username, password, role)
         query = "INSERT INTO users (name, userpass, role) VALUES (%s,%s,%s)"
         self.cursor.execute(query, value)
         self.db.commit()
-        log_change(
+        Logs.log_change(
             f"Created new user {username} with role {role}",
             user="admin"
         )
-        # print(token)
-        # print(fernet.decrypt(token))
-        # print(str(fernet.decrypt(token))[2:-1])
 
     def delete_user(self, username, executer = "admin"):
         self.cursor.execute(f"DELETE FROM users WHERE name = '{username}'")
         self.db.commit()
-        log_change(
+        Logs.log_change(
             f"Deleted user {username}",
             user = executer
         )
         
         pass
+    
+    def get_users(self):
+        self.cursor.execute("SELECT name, userpass, role FROM users")
+        Logs.log_change("Requested all users", user = "admin")
+        return self.cursor.fetchall()
+    
+    def validate_user_login(self, user, password):
+        self.cursor.execute(f"SELECT name, userpass from users WHERE name = %s", (user,))
+        detail = self.cursor.fetchone()
+        if detail is None or detail[1] != password:
+            return False
+        else:
+            return True
 
-    def set_password(self, old_password, new_password):
-        pass
+    def set_password(self, user, old_password, new_password):
+        self.cursor.execute(f"SELECT name, userpass from users WHERE name = %s", (user,))
+        detail = self.cursor.fetchone()
+        if old_password != detail[1]:
+            return False
+        self.cursor.execute(f"UPDATE users SET userpass = %s WHERE name = %s", (new_password, user))
+        self.db.commit()
+        return True
 
-    def get_user(self, user):
-        pass
 
 
-db = UserManagement()
-cursor = db.cursor
-for i in range(10):
-    db.delete_user("test")
-
-k.close()
+if __name__ == "__main__": 
+    db = UserManagement("system")
 
 
